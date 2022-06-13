3ab7d29053fe3dbc1cde761a982ab03561eec4b6
William Santiago <96854760+Ix1x0x3@users.noreply.github.com>
2022-06-13 11:10:39 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 const Terminal = document.getElementById('terminal');
 
+// when terminal text is changed
+Terminal.addEventListener('input', (e) => {
+    // get the text
+    const text = e.target.value;
+
+    console.log(text);
+})
